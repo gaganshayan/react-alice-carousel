926d15ec10aa02eca8fe0ea4b4187d34b8d60f4e
Maksim Marinich <mmarinich@Admins-MacBook-Pro.local>
2020-10-07 17:49:33 +0300

    upd navigation

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import './styles.scss';
 
+const setHash = (hash = '') => {
+	try {
+		history.pushState ? history.pushState(null, '', hash) : (location.hash = hash);
+	} catch (e) {
+		//
+	}
+};
+
 const Navigation = ({ onclick, scheme }) => {
 	return scheme.length > 1 ? (
 		<nav className="navigation">
 			{scheme.map(({ id, title }) => {
 				return (
-					<a href={`/#${id}`} key={id} className="navigation-item" onClick={() => onclick({ id, title })}>
+					<a
+						href={`/#${id}`}
+						key={id}
+						className="navigation-item"
+						onClick={(e) => {
+							e.preventDefault();
+							setHash(`#${id}`);
+							onclick({ id, title });
+						}}
+					>
 						{title}
 					</a>
 				);
